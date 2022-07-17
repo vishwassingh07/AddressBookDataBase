206b357234c8ff6b3e58b1f767e4eb21669e0d5f
Vishwas <baghel.vishwas0@gmail.com>
2022-07-17 17:08:08 +0530

    [Vishwas]add: Inserting New Contact Details Into Address Book Service

---
+++ SQLQuery3.sql
@@ -0,0 +1,6 @@
+-------------- UC3- Inserting New Contact In AddressBook Table ----------
+Insert Into AddressBook_Table
+Values('Vishwas', 'Singh','Marathalli','Bangalore','Karnataka', 560066, 9424888999,'baghel.vishwas0@gmail.com'),
+('Abhishek','Jadhav','Link Road','Thane', 'Maharashtra', 456321, 7896541364, 'abhi@gmail.com'),
+('Vivek','Baghel', 'Andheri', 'Mumbai', 'Maharashtra', 563214, 6547893214, 'vivek@gmail.com'),
+('Vikas','Khote', 'Whitefield', 'Bangalore', 'Karnataka', 563894, 8965231474, 'vikask@gmail.com');
