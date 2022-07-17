c9fdc1e74dbb59d106fcfe83634179aef9afb69c
Vishwas <baghel.vishwas0@gmail.com>
2022-07-17 17:23:42 +0530

    [Vishwas]add: Editing Existing Contact Details In AddressBook Table

---
+++ SQLQuery4.sql
@@ -0,0 +1,4 @@
+-------------- UC4- Edit Details Into Existing Contact ------------
+ Update AddressBook_Table Set City = 'Delhi',State = 'Delhi' Where FirstName = 'Vishwas';
+ Update AddressBook_Table Set Email = 'updated@gmail.com' Where FirstName = 'Abhishek';
+
