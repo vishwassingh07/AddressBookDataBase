e8a7d360ebfe395d19c1167aef04e2a8bbca283b
Vishwas <baghel.vishwas0@gmail.com>
2022-07-17 16:47:28 +0530

    [Vishwas]add: Creating Table For Address Book Service

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+-------------- UC2- Creating Table For AddressBook ------------
+
+Create Table AddressBook_Table(
+FirstName varchar(200),
+LastName varchar(200),
+Address varchar(200),
+City varchar(200),
+State varchar(200),
+Zipcode bigint,
+PhoneNumber bigint,
+Email varchar(200)
+
+);
+Select * From AddressBook_Table;
