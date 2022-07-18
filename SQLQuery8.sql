2638029838fe0eff1b036642c08bf7dcd5426a3a
Vishwas <baghel.vishwas0@gmail.com>
2022-07-18 12:08:59 +0530

    [Vishwas]add: Retrieving Sorted Contact List By Name Or City

---
+++ SQLQuery8.sql
@@ -0,0 +1,7 @@
+----------- UC8- Retrieving Sorted Contact Details ---------
+
+------ Sorted Alphabetically By First Name -----
+Select * From AddressBook_Table Order By FirstName;
+
+------ Sorted Alphabetically By City Name -------
+Select * From AddressBook_Table Order By City;
