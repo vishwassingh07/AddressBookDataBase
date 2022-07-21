1fb4bd3ca0fb520c0fc70cd1b7bed33371ae51c1
Vishwas <baghel.vishwas0@gmail.com>
2022-07-21 23:24:11 +0530

    [Vishwas]add: Adding Same Contact In Two Contact Type

---
+++ SQLQuery12.sql
@@ -0,0 +1,6 @@
+----------- UC 11 - Adding Contact In Both The Contact Type ------------
+
+Insert Into AddressBook_Table
+Values('Virendra','Sonekar','Ekta Nagar','Delhi','Delhi',110011,7896541235,'virendra@gmail.com','Friend'),
+('Virendra','Sonekar','Ekta Nagar','Delhi','Delhi',110011,7896541235,'virendra@gmail.com','Family');
+Select * From AddressBook_Table;
